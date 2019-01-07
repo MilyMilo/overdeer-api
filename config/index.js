906bf7c583f09eeecbab5e59c401fba1d31437ae
Miłosz Skaza <github@milymilo.cc>
2019-01-07 21:18:27 +0100

    Fix testing in CI (#1)

--- config/index.js
+++ config/index.js
@@ -1,4 +1,8 @@
-if (process.env.NODE_ENV === "production") {
+if (
+  process.env.NODE_ENV === "production" ||
+  // CircleCI should use the same variables as prod since they both come from ENV
+  process.env.NODE_ENV === "circle"
+) {
   module.exports = require("./prod");
 } else {
   module.exports = require("./dev");
